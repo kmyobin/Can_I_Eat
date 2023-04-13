8d3ab2f19e71206e79d791d587a1ed495eabcc1d
doggopawer <doggopawer@gmail.com>
2023-04-13 17:29:14 +0900

    feat : SearchForm 스타일 완성
    SearchFormWrapper, SearchHeading, SearchInput, SearchButton css 스타일 완성

--- src/pages/SearchPage/SearchForm.tsx
+++ src/pages/SearchPage/SearchForm.tsx
@@ -1,28 +1,63 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const SearchFormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  
   width: 40%;
   height: 600px;
-  border: 1px solid;
-  
-  display: flex;
-  justify-content: center;
-`;
-const ContentBox = styled.div`
-  width: 80%;
-  height: 100%;
-  border: 1px solid;
 `;
 
+const SearchHeading = styled.h1`
+  box-sizing: border-box;
+  width: 100%;
+  height: 140px;
+  margin: 0;
+  
+  font-size: 36px;
+  font-family: NotoSansKR-700;
+  font-weight: 500;
+  text-align: center;
+  line-height: 140px;
+`;
+const SearchInput = styled.input`
+  box-sizing: border-box;
+  width: 400px;
+  height: 55px;
+  border: none;
+  border-radius: 15px;
+  background: #F5F5F5;
+`;
+const SearchButton = styled.button`
+  box-sizing: border-box;
+  width: 351px;
+  height: 50px;
+  background:#00B578;
+  border: none;
+  border-radius: 4px;
+  
+  font-size: 18px;
+  font-family: NotoSansKR-700;
+  font-weight: 500;
+  color: #fff;
+`;
 
 function SearchForm() {
     return (
-        <Wrapper>
-            <ContentBox>
+        <SearchFormWrapper>
+            <SearchHeading>
+                이제 식품을 검색해주세요!
+            </SearchHeading>
+            <SearchInput>
 
-            </ContentBox>
-        </Wrapper>
+            </SearchInput>
+            <SearchButton>
+                검색
+            </SearchButton>
+        </SearchFormWrapper>
     );
 }
 
