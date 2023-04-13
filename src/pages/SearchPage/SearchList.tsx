5555b318095f567048b743324b5a67f8013233c8
doggopawer <doggopawer@gmail.com>
2023-04-13 18:49:54 +0900

    feat : SearchList 스타일 완성
    SearchListWrapper, SearchScrollBox, SearchItem 스타일 완성

--- src/pages/SearchPage/SearchList.tsx
+++ src/pages/SearchPage/SearchList.tsx
@@ -1,28 +1,62 @@
 import React from "react";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
+const SearchListWrapper = styled.div`
+  display: flex;
+  justify-content: center;
   width: 60%;
   height: 600px;
-  border: 1px solid;
-  
-  justify-content: center;
-  display: flex;
 `;
-const ContentBox = styled.div`
+const SearchScrollBox = styled.div`
   width: 80%;
   height: 100%;
-  border: 1px solid;
+  padding-right: 20px;
+  overflow-y: scroll;
+
+  /* 스크롤바 너비 조정 */
+  ::-webkit-scrollbar {
+    width: 6.5px;
+  }
+  /* 스크롤바 현재 위치 */
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: #00B57840;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #00B578;
+  }
+  /* 스크롤바 전체 부분 */
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background: #ddd;
+  }
+`;
+const SearchItem = styled.div`
+  width: 100%;
+  height: 68px;
+  margin-bottom: 25px;
+  background: #F5F5F5;
+  border-radius: 10px;
 `;
 
-function SearchContent() {
+function SearchList() {
     return (
-        <Wrapper>
-            <ContentBox>
+        <SearchListWrapper>
+            <SearchScrollBox>
+                <SearchItem>
 
-            </ContentBox>
-        </Wrapper>
+                </SearchItem>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+                <SearchItem/>
+            </SearchScrollBox>
+        </SearchListWrapper>
     );
 }
 
-export default SearchContent;
+export default SearchList;
