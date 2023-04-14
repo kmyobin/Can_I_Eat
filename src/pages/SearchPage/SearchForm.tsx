662b173fa0503757b4a9a5e62f8c3aaaad289260
doggopawer <doggopawer@gmail.com>
2023-04-14 21:41:27 +0900

    style : 코드 설명 주석 추가
    데이터 페칭 코드에 대한 의도를 설명하는 텍스트 추가

--- src/pages/SearchPage/SearchForm.tsx
+++ src/pages/SearchPage/SearchForm.tsx
@@ -72,12 +72,14 @@ function SearchForm({handleSearchResultsChange, handleIsLoadingToggle} : SearchF
         handleIsLoadingToggle();
         try {
             const {data:{C002:{row}}} =  await axios.get(makeFoodRawMaterialURL(process.env.REACT_APP_RAW_MATERIAL_KEY, searchKeyword));
+            // 원재료 API 에서 가져온 데이터의 수와 상관없이 이미지 API 를 일괄적으로 한번만 호출 한다.(axios.all)
             const ids = row.map(({PRDLST_REPORT_NO}: SearchResult) => PRDLST_REPORT_NO);
             const images = await axios.all(ids.map((id:string) => axios.get(makeFoodImgURL(process.env.REACT_APP_FOOD_IMAGE_KEY, id))));
             const imgUrls = images.map(({data:{body: items}}:any)=> items[0]?.item.imgurl1);
             const results = row.map((item:SearchResult, index:number) => ({...item, IMG_URL: imgUrls[index]}));
             handleSearchResultsChange(results);
         } catch (e) {
+            // 데이터가 없을시 Array.map()으로 인한 TypeError 가 발생되는 경우를 처리하기 위함.
             if (e instanceof TypeError) {
                 handleSearchResultsChange([]);
                 console.log('TypeError가 발생했습니다:', e.message);
