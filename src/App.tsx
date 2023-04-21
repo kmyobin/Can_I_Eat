ff2b4097459af86e64e0fc9d0b845ae9eb3b0bb0
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-04-21 15:45:25 +0900

    fix : useState 추가 searchResult에 대한 useState 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import SelectPage from "./pages/SelectPage";
 import SearchPage from "./pages/SearchPage";
+import { SearchResult } from "type";
 
 function App() {
+  const [searchResults, setSearchResults] = useState<SearchResult[]>();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/select" element={<SelectPage />} />
         <Route path="/search" element={<SearchPage />} />
+        {/*<Route path="/result/:foodId" element={<ResultPage/>}/>*/}
       </Routes>
     </BrowserRouter>
   );
